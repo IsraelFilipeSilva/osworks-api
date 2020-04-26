c5709ba3a90de09be5a6bf95a80f11562181345f
IsraelFilipe <israel.filipe@outlook.com>
2020-04-26 22:15:48 +0200

    Implementacao do ModelMapper, separacao do Domain Model e Representation model com padrao DTO

--- src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -1,6 +1,6 @@
 package com.algaworks.osworks.domain.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
