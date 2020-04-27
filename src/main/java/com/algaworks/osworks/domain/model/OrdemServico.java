47d87051c4a5385770ab24fcd7b7bf44ba728fbf
IsraelFilipe <israel.filipe@outlook.com>
2020-04-27 03:30:38 +0200

    Model Mapper e subrecursos e acoes nao CRUD na Api

--- src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -1,8 +1,9 @@
 package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -11,15 +12,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
+import javax.persistence.OneToMany;
 
-import com.algaworks.osworks.domain.ValidationGroups;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.algaworks.osworks.exception.NegocioException;
 
 @Entity
 public class OrdemServico {
@@ -39,6 +34,9 @@ public class OrdemServico {
 
 	private OffsetDateTime dataAbertura;
 	private OffsetDateTime dataFinalizacao;
+
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -96,6 +94,14 @@ public class OrdemServico {
 		this.dataFinalizacao = dataFinalizacao;
 	}
 
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -119,6 +125,23 @@ public class OrdemServico {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public boolean podeSerFinalizada() {
+		return StatusOrdemServico.ABERTA.equals(getStatus());
+	}
+
+	public boolean naoPodeSerFinalizada() {
+		return !podeSerFinalizada();
+	}
+
+	public void finalizar() {
+		if (naoPodeSerFinalizada()) {
+			throw new NegocioException("Ordem de serviço nao pode ser finalizada");
+		}
+
+		setStatus(StatusOrdemServico.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
 	}
 
 }
---
+++ src/main/java/com/algaworks/osworks/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,12 @@
+package com.algaworks.osworks.exception;
+
+public class EntidadeNaoEncontradaException extends NegocioException{
+
+	private static final long serialVersionUID = 5380491429034111939L;
+
+	public EntidadeNaoEncontradaException(String message) {
+		super(message);
+		
+		
+	}
+}
